bb32d0c6372c2cb0055cc46e6643bd89bd666ef4
Cristian Duque <dev.cristian.duque@gmail.com>
2018-01-29 17:07:31 -0500

    environment

--- remote_config/main.py
+++ remote_config/main.py
@@ -7,11 +7,11 @@ client = boto3.client('lambda')
 
 
 @lru_cache(maxsize=32)
-def get_config_key(key):
+def get_config_key(key, env="PRD"):
     response = client.invoke(
         FunctionName='remote-config',
         InvocationType='RequestResponse',
-        Payload=json.dumps({'key': key}),
+        Payload=json.dumps({'key': key, 'env': env}),
     )
 
     return json.loads(response['Payload'].read().decode('utf-8')).get('value')
